28327ebc1acc2508b06f955233b9ed1c43f61686
Paul <isakura313@gmail.com>
2021-01-15 21:09:21 +0300

    asdasd

--- components/Main.vue
+++ components/Main.vue
@@ -2,9 +2,54 @@
 <div class='gallery'>
   <section class='section'>
     <div class='columns is-multiline'>
-
+      <Card
+        v-for = 'image in photos'
+        :key =  'image.id'
+        :srcimage = 'image.thumb'
+        @showGallery = "showLightBox(image.index)"
+      />
     </div>
+    <LightBox
+      ref='lightGallery'
+      :media='photos'
+      :showLightBox = "false"
+      />
   </section>
+  <div class='section is-centered'>
+    <b-pagination
+      v-model="current"
+      :total="total"
+      :range-before="rangeBefore"
+      :range-after="rangeAfter"
+      :size="size"
+      :simple="isSimple"
+      :rounded="isRounded"
+      :per-page="perPage"
+      :icon-prev="prevIcon"
+      :icon-next="nextIcon"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+      @change = "getInfo" >
+      <b-pagination-button
+        slot='previous'
+        slot-scope='props'
+        :page="props.page"
+        tag="router-link"
+        :to="`/documentation/pagination#page${props.page.number}`">
+        Previous
+      </b-pagination-button>
+      <b-pagination-button
+        slot='previous'
+        slot-scope='props'
+        :page="props.page"
+        tag="router-link"
+        :to="`/documentation/pagination#page${props.page.number}`">
+        Previous
+      </b-pagination-button>
+    </b-pagination>
+  </div>
 </div>
 </template>
 
@@ -13,7 +58,47 @@ import LightBox from 'vue-image-lightbox'
 import Card from '../components/Card'
 
 export default {
-  name: 'Main'
+  name: 'Main',
+  components: {
+    Card,
+    LightBox
+  },
+  props:{
+    photos: {
+      type: Array,
+      required: true
+    },
+    total: {
+      type: Number,
+      default: 300
+    },
+    imageCount: {
+      type: Number,
+      default: 30
+    }
+  },
+  methods:{
+    showLightBox(idPhoto){
+      this.lightToShow = !this.lightToShow
+      this.$refs.lightGallery.lightBoxOn = true
+      this.$refs.lightGallery.showImage(idPhoto)
+    },
+    getInfo (){
+      this.$emit("changePage", this.current)
+    }
+  },
+  data(){
+    return {
+      current: 1,
+      size: '',
+      rangeBefore: 3,
+      rangeAfter: 1,
+      isSimple: false,
+      isRounded: false,
+      prevIcon: 'chevron-left',
+      nextIcon: 'chevron-right'
+    }
+  }
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,61 @@
 <template>
-  <section class="section">
-  <div class='columns is-multiline'>
-    <Card
-    v-for = 'photo in data.photos'
-    :key =  'photo.id'
-    :srcimage = 'photo.src.large'
+  <section class='section'>
+    <Main
+      :photos='media'
+      :total = 'total'
+      :image-count='15'
+      @changePage = "getInfo"
     />
-  </div>
   </section>
 </template>
 
 <script>
 import Card from '~/components/Card'
+import Main from '~/components/Main'
 
 export default {
   name: 'HomePage',
 
   components: {
+    Main,
     Card
-  }, async fetch () {
-    this.data = await fetch('https://api.pexels.com/v1/curated', {
+  }, async fetch() {
+    this.media = await fetch(`https://api.pexels.com/v1/curated?page=${this.page}`, {
       method: 'GET',
       headers: {
         Authorization: '563492ad6f91700001000001c6dc5c5329904df0936ea995dc4d7209'
       }
     }).then(res => res.json())
+    .then(function(value){
+      const tempGallery = []
+      for (let i = 0; i < value.photos.length; i++){
+        tempGallery.push(
+          {
+            index: i,
+            thumb: value.photos[i].src.large,
+            src: value.photos[i].src.large,
+          })
+      }
+      return tempGallery
+    })
   },
   data() {
-    return{
-      data: []
+    return {
+      page: 1,
+      total: 300,
+      media: [
+        {
+          thumb: 'https://images.pexels.com/photos/3885950/pexels-photo-3885950.jpeg',
+          src: 'https://images.pexels.com/photos/3885950/pexels-photo-3885950.jpeg'
+        }
+      ]
+    }
+  },methods:{
+    getInfo(page){
+      this.page = page
+      this.$fetch()
+      this.show = true
+
     }
   }
 }
