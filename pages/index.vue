3df0366a7789a00b3eee9f6a9fb2f66c10b8fa3f
Paul <isakura313@gmail.com>
2021-06-12 12:53:42 +0300

    fix it and move to asyncData

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,16 @@
 <template>
-<v-main>
-  <v-container>
-  <v-row>
-    <!-- <Main
-      :photos='media'
-      :total = 'total'
-      :image-count='15'
-      @changePage = "getInfo"
-    /> -->
-        <v-col v-for="image in media"  :key="image.index">
+  <v-main>
+    <v-container>
+      <v-row>
+        <v-col v-for="image in dataImg" :key="image.index">
           <v-img
-          :src="image.thumb"
-          max-height="150"
-          max-width="250"
-          @click="hello"/>
+            :src="image.thumb"
+            max-height="150"
+            max-width="250"
+            @click="hello"
+          />
         </v-col>
-  </v-row>
+      </v-row>
     </v-container>
   </v-main>
 </template>
@@ -29,48 +24,53 @@ export default {
 
   components: {
     Main,
-    Card
-  }, async fetch() {
-    this.media = await fetch(`https://api.pexels.com/v1/curated?page=${this.page}`, {
-      method: 'GET',
-      headers: {
-        Authorization: '563492ad6f91700001000001c6dc5c5329904df0936ea995dc4d7209'
-      }
-    }).then(res => res.json())
-    .then(function(value){
-      const tempGallery = []
-      for (let i = 0; i < value.photos.length; i++){
-        tempGallery.push(
-          {
-            index: i,
-            thumb: value.photos[i].src.large,
-            src: value.photos[i].src.large,
-          })
-      }
-      return tempGallery
-    })
+    Card,
   },
-  data() {
+
+  data:function() {
     return {
       page: 1,
       total: 300,
       media: [
         {
-          thumb: 'https://images.pexels.com/photos/3885950/pexels-photo-3885950.jpeg',
-          src: 'https://images.pexels.com/photos/3885950/pexels-photo-3885950.jpeg'
-        }
-      ]
+          thumb:
+            'https://images.pexels.com/photos/3885950/pexels-photo-3885950.jpeg',
+          src: 'https://images.pexels.com/photos/3885950/pexels-photo-3885950.jpeg',
+        },
+      ],
     }
-  },methods:{
-    getInfo(page){
+  },
+  async asyncData({ $axios }) {
+    const photo = await $axios.$get(
+      'https://api.pexels.com/v1/curated?page=1',
+      {
+        method: 'GET',
+        headers: {
+          Authorization:
+            '563492ad6f91700001000001c6dc5c5329904df0936ea995dc4d7209',
+        },
+      }
+    )
+    console.log(photo.photos)
+    const dataImg= photo.photos.map((item, index) => {
+      return {
+        index: index,
+        thumb: item.src.large,
+        src: item.src.large,
+      }
+    }) 
+    return {dataImg};
+  },
+
+  methods: {
+    getInfo(page) {
       this.page = page
       this.$fetch()
       this.show = true
-
     },
-    hello(){
-      alert('hello');
-    }
-  }
+    hello() {
+      alert('hello')
+    },
+  },
 }
 </script>
